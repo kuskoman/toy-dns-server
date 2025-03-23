1981c92e72a0eb8ba0f8c233afbdbe98efebd847
Jakub Surdej <jakub.surdej@shift4.com>
2025-03-23 12:36:52 +0100

    Rewrite flags config

--- tests/utils/flags_test.py
+++ tests/utils/flags_test.py
@@ -1,19 +1,36 @@
 import unittest
 from unittest.mock import patch
-from toy_dns_server.utils.flags import parse_flags, get_default_config_path
+from toy_dns_server.utils.flags import FlagsManager
+
 
 class TestFlags(unittest.TestCase):
 
     @patch("sys.argv", ["flags.py"])
-    def test_default_config_path(self):
-        """Test default config path based on OS"""
-        expected_path = get_default_config_path()
-        self.assertEqual(parse_flags()["config"], expected_path)
+    @patch("platform.system", return_value="Linux")
+    def test_default_config_path_unix(self, mock_platform):
+        manager = FlagsManager()
+        flags_config = manager.get_flags_config()
+        self.assertEqual(flags_config.default_config_path, "/etc/toy_dns_server/config.yml")
+        self.assertEqual(flags_config.user_config_path, "/etc/toy_dns_server/config.yml")
 
-    @patch("sys.argv", ["flags.py", "--config", "/tmp/test.yml"])
-    def test_custom_config(self):
-        """Test custom config file flag"""
-        self.assertEqual(parse_flags()["config"], "/tmp/test.yml")
+    @patch("sys.argv", ["flags.py"])
+    @patch("platform.system", return_value="Windows")
+    @patch.dict("os.environ", {"APPDATA": "C:\\Users\\Test\\AppData\\Roaming"})
+    def test_default_config_path_windows(self, mock_platform):
+        manager = FlagsManager()
+        flags_config = manager.get_flags_config()
+        expected_path =FlagsManager.DEFAULT_WINDOWS_CONFIG
+        self.assertEqual(flags_config.default_config_path, expected_path)
+        self.assertEqual(flags_config.user_config_path, expected_path)
+
+    @patch("sys.argv", ["flags.py", "--config", "/tmp/custom.yml"])
+    @patch("platform.system", return_value="Linux")
+    def test_custom_config_path(self, mock_platform):
+        manager = FlagsManager()
+        flags_config = manager.get_flags_config()
+        self.assertEqual(flags_config.user_config_path, "/tmp/custom.yml")
+        self.assertEqual(flags_config.default_config_path, "/etc/toy_dns_server/config.yml")
+
 
 if __name__ == "__main__":
     unittest.main()
