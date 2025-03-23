2dd6a7a9e25ef137d5ba140c4e42b5620ae7bd78
Jakub Surdej <kubasurdej@gmail.com>
2025-03-23 16:02:37 +0100

    Handle empty requests in http_handler

--- toy_dns_server/server/doh/http_handler.py
+++ toy_dns_server/server/doh/http_handler.py
@@ -13,7 +13,6 @@ class DNSOverHTTPHandler(BaseHTTPRequestHandler):
         super().__init__(request, client_address, server)
 
     def do_POST(self):
-        self._logger.debug("Handling DoH query")
         content_type = self.headers.get("Content-Type")
         if content_type != "application/dns-message":
             self.send_error(415, "Unsupported Media Type")
@@ -21,7 +20,14 @@ class DNSOverHTTPHandler(BaseHTTPRequestHandler):
 
         try:
             content_length = int(self.headers.get("Content-Length", 0))
+            if content_length == 0:
+                self.send_error(400, "No DNS query data provided")
+                return
+
             query_data = self.rfile.read(content_length)
+            if not query_data:
+                self.send_error(400, "Empty DNS query data")
+                return
 
             self._logger.debug(f"Received DoH query from {self.client_address[0]}:{self.client_address[1]}")
             dns_request = DNSRecord.parse(query_data)
@@ -34,9 +40,11 @@ class DNSOverHTTPHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Length", str(len(response_data)))
             self.end_headers()
             self.wfile.write(response_data)
+
         except Exception as e:
             self._logger.error(f"Failed to handle DoH query: {e}\n{traceback.format_exc()}")
             self.send_error(500, "Internal Server Error")
+
 
     def log_message(self, format, *args):
         self._logger.debug(format % args)
